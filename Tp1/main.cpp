b2b7863cbca0228c744d1cda15e99522b15c0297
hamed-7 <62188818+hamed-7@users.noreply.github.com>
2021-01-15 19:20:28 +0100

    commit main new code tp1

--- Tp1/main.cpp
+++ Tp1/main.cpp
@@ -13,6 +13,7 @@ unsigned int size_len ;
 FILE *fd; 
 public:
     fichier(/* args */);
+    fichier(char*,char*,unsigned int);
     ~fichier();
 
     void Set_Noun(char* );
@@ -36,9 +37,59 @@ fichier::fichier(/* args */)
  cout<<"construction par defaut";
 }
 
+ fichier::fichier(char* name,char*mod  ,unsigned int size)
+{
+ this->Name=new char [sizeof name];
+ Name=name;
+ this->Mode= new char [sizeof mod];
+ Mode=mod;
+ this->size_len=size;
+  cout<<"construction specifique";
+// this->fd=fopen(Name,Mode);
+// cout<<fd;
+}
+
 fichier::~fichier()
 {
 cout<<"\n bye bye";
 fclose(this->fd);
 }
+//Getters###
+unsigned int fichier::Get_len(){
+    return this->size_len;
+}
+char * fichier::Get_Noun(){
+return(this->Name);
+}
+char * fichier::Get_Mode(){
+return(this->Mode);
+}
 
+void fichier::Set_Noun(char*name){
+this->Name=new char [sizeof name];
+this->Name=name;
+
+ 
+//cout<<"name"<<Name<<endl;
+}
+void fichier::Set_Mode(char*mode){
+this->Mode=new char [sizeof mode];
+this->Mode=mode;
+
+//cout<<"Mode"<<Mode<<endl;
+}
+
+void fichier::Set_Len(unsigned int num){
+this->size_len=num;
+}
+
+int main(){
+//fichier test("test.txt","r+",10);
+fichier test;
+test.Set_Noun("ach.txt");
+test.Set_Mode("r+");
+test.Set_Len(5);
+ cout<<test.Get_Mode()   <<"mode    "<<test.Get_Noun()<<"noun   "<<test.Get_len()<<"length";
+    return 0 ;
+
+}
