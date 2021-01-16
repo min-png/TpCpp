80f6b3cd1d78b89e012841994b660ce3569940ef
hamed-7 <hamed-7@github.com>
2021-01-16 12:50:14 +0100

    function str_c pour minimiser les retendances de conversion

--- Tp1/main.cpp
+++ Tp1/main.cpp
@@ -26,8 +26,16 @@ public:
 
     void Ecrire(char*);
     void lire();
-
+    friend char * str_c(string);
 };
+
+char* str_c(string s)
+{
+char* cha = new char[s.size() + 1];
+std::copy(s.begin(), s.end(), cha);
+cha[s.size()] = '\0';
+return cha;
+}
 
 fichier::fichier(/* args */)
 {
@@ -41,14 +49,9 @@ fichier::fichier(/* args */)
 
  fichier::fichier(string name,string mod  ,unsigned int size)
 {
-this->Name = new char[name.size() + 1];
-std::copy(name.begin(), name.end(), Name);
-this->Name[name.size()] = '\0';
+this->Name = str_c(name);
 
-this->Mode = new char[mod.size() + 1];
-std::copy(mod.begin(), mod.end(), Mode);
-this->Mode[mod.size()] = '\0';
-
+this->Mode = str_c(mod);
 this->size_len=size;
   cout<<"construction specifique";
 // this->fd=fopen(Name,Mode);
@@ -71,18 +74,14 @@ char * fichier::Get_Mode(){
 return(this->Mode);
 }
 
-void fichier::Set_Noun(string name){
-this->Name=new char [sizeof name];
-//this->Name=*name;
-
- 
-//cout<<"name"<<Name<<endl;
+void fichier::Set_Noun(string name)
+{
+    this->Name=str_c(name);
 }
-void fichier::Set_Mode(string mode){
-this->Mode=new char [sizeof mode];
-//this->Mode=mode;
 
-//cout<<"Mode"<<Mode<<endl;
+void fichier::Set_Mode(string mod)
+{
+    this->Mode=str_c(mod);
 }
 
 void fichier::Set_Len(unsigned int num){
@@ -90,13 +89,14 @@ this->size_len=num;
 }
 
 int main(){
+    
 fichier test("test.txt","r+",10);
 
-//fichier test;
-// test.Set_Noun("ach.txt");
-// test.Set_Mode("r+");
-// test.Set_Len(5);
- cout<<test.Get_Mode()   <<"mode    "<<test.Get_Noun()<<"noun   "<<test.Get_len()<<"length";
-    return 0 ;
+// fichier test;
+//  test.Set_Noun("ach.txt");
+//  test.Set_Mode("r+");
+//  test.Set_Len(5);
+//  cout<<test.Get_Mode()   <<"mode    "<<test.Get_Noun()<<"noun   "<<test.Get_len()<<"length";
+//   return 0 ;
 
 }
